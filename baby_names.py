127e6ffd3a6b12508107670f5787e89a9830b48c
Frederick Kim <freddy.kim8@gmail.com>
2019-12-31 00:42:54 -0800

    Functional server but bad alg

--- baby_names.py
+++ baby_names.py
@@ -2,40 +2,29 @@ from metaphone import doublemetaphone
 import pandas as pd
 from fuzzywuzzy import fuzz
 
-#get name file
-babyNamesRaw = pd.read_csv("C:\\Users\\jenni\\Downloads\\Popular_Baby_Names.csv")
-babyNames = df_no_repeats(babyNamesRaw)
-
-#get user input
-entered_name = doublemetaphone(input())
-gender = input('Female or Male? ').upper()
-
 #functions to get rid of repeats
 def df_no_repeats(df):
     dupes = df.duplicated()
     df2 = df[~dupes]
     return df2
 
-def list_no_repeats(li):
-    final_list = []
-    for i in li:
-        if i.capitalize() not in final_list:
-            final_list.append(i.capitalize())
-    return final_list
+def find_names(name, gender):
+    #get name file
+    babyNamesRaw = pd.read_csv("Popular_Baby_Names.csv")
+    babyNames = df_no_repeats(babyNamesRaw)
 
-#getting the names
-babyNames_gender = babyNames[babyNames.Gender == gender]
-babyNames_gender = df_no_repeats(babyNames_gender)
+    entered_name = doublemetaphone(name)
 
-print_names = []
-for i in babyNames_gender["Child's First Name"]:
-    ratio = fuzz.ratio(doublemetaphone(entered_name), doublemetaphone(match_name))
-    if ratio >= 80:
-        print_names.append(i)
+    #getting the names
+    babyNames_gender = babyNames[babyNames.Gender == gender.upper()]
+    babyNames_gender = df_no_repeats(babyNames_gender)
 
-final_list = list_no_repeats(print_names)
-print (final_list)
+    close_names = set()
+    for match_name in babyNames_gender["Child's First Name"]:
+        ratio = fuzz.ratio(entered_name, doublemetaphone(match_name))
+        if ratio >= 80:
+            close_names.add(match_name.capitalize())
 
-
-
-
+    result = list(close_names)
+    result.sort()
+    return result
--- server.py
+++ server.py
@@ -1,13 +1,16 @@
-from flask import *
+from flask import Flask, request, render_template
+from baby_names import find_names
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    name = request.args.get("name", "World")
-    gender = request.args.get("gender", "Male")
-    return render_template("index.html", name = name, gender = gender)
-    # return f'Hello, {escape(name)}!'
+    name = request.args.get("name", None)
+    gender = request.args.get("gender", None)
+    results = []
+    if name is not None and name != '' and gender is not None:
+      results = find_names(name, gender)
+    return render_template("index.html", name = name, gender = gender, results = results)
 
 if __name__ == '__main__':
   app.run(debug = True)
